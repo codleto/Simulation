b0e2b2cd1fe0795460c5797b795ea1d9c5dee334
codleto <codleto@gmail.com>
2025-10-23 21:57:10 +0300

    add : class Creature

---
+++ src/Simulation/Animal/Creature.java
@@ -0,0 +1,11 @@
+package Simulation.Animal;
+
+import Simulation.Cordinates;
+import Simulation.Entity;
+
+public abstract class Creature extends Entity {
+    int id;
+    public Creature move;
+    //energy
+
+}
