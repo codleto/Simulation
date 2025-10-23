d5e62f432a7a914039ec36ced9770598b864b9af
codleto <codleto@gmail.com>
2025-10-23 21:59:32 +0300

    feat: add Renderer and Map methods for sprite rendering

--- src/Simulation/Map.java
+++ src/Simulation/Map.java
@@ -1,4 +1,24 @@
 package Simulation;
 
-public  class Map {
+import Simulation.Animal.Creature;
+import Simulation.Animal.Herbivore;
+import Simulation.Animal.Predator;
+
+import java.util.HashMap;
+
+public class Map {
+    HashMap<Cordinates, Entity> map = new HashMap<>();
+
+
+    public void setEntity(Cordinates cordinates, Entity entity) {
+        map.put(cordinates, entity);
+    }
+
+    public void change(Entity entity, int id){
+
+    }
+    public Entity getMap(File file, int a) {
+        return map.get(new Cordinates(file, a));
+    }
 }
+
