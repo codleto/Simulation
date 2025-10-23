6051fa475d58fbcb58356d774b757f8dcd7ef03c
codleto <codleto@gmail.com>
2025-10-23 21:58:30 +0300

    feat(renderer): implement Renderer method and add sprite rendering

--- src/Simulation/Renderer.java
+++ src/Simulation/Renderer.java
@@ -1,10 +1,20 @@
 package Simulation;
 
 public class Renderer {
-    public void renderer(Map map){
-        for(int rank = 10; rank >= 10; rank--){
 
+    public static void renderer(Map map) {
+        for (int rank = 10; rank >= 1; rank--) {
+            System.out.println();
+            for (File file : File.values()) {
+                if(map.getMap(file, rank) == null){
+                    System.out.print("\u001b[42m   ");
+                } else {
+                    System.out.print("\u001b[41m \uD83D\uDC3A");
+                }System.out.print("\u001B[0m");
+
+
+            }
         }
-    }
 
+    }
 }
