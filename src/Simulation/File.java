ca41224e475df451cf7ea94b852f0660129524e5
codleto <codleto@gmail.com>
2025-10-24 17:53:17 +0300

    feat: rename Vertical

--- src/Simulation/File.java
+++
@@ -1,5 +0,0 @@
-package Simulation;
-
-public enum File {
-    A, B, C, D, E, F, G, H, I, J;
-}
---
+++ src/Simulation/Vertical.java
@@ -0,0 +1,10 @@
+package Simulation;
+
+public enum Vertical {
+    A, B, C, D, E, F, G, H, I, J;
+
+    public void Ordinal(Vertical vertical){
+        Vertical[] v = values();
+
+    }
+}
