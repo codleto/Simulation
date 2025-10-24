941c4d1b39ea9367d972634512d571196b552bb1
codleto <codleto@gmail.com>
2025-10-24 17:54:17 +0300

    feat: Createre add new method position

--- src/Simulation/Animal/Creature.java
+++ src/Simulation/Animal/Creature.java
@@ -2,10 +2,17 @@ package Simulation.Animal;
 
 import Simulation.Cordinates;
 import Simulation.Entity;
+import Simulation.Vertical;
 
 public abstract class Creature extends Entity {
     int id;
     public Creature move;
     //energy
+    public Cordinates position(Creature creature){
+       return creature.cordinates;
+    }
+
+    public void setPosititon(Cordinates cordinates, Creature creature, Vertical x, int y){
+    }
 
 }
