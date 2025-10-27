e29e6be058a9aef00f073a90fa27154416a86344
codleto <codleto@gmail.com>
2025-10-27 17:53:29 +0300

    feat: new constructor in Herbivore

--- src/Simulation/Animal/Herbivore.java
+++ src/Simulation/Animal/Herbivore.java
@@ -1,9 +1,12 @@
 package Simulation.Animal;
 
 public class Herbivore extends Creature {
-    String skin = "\uD83D\uDC11";
-
     public Herbivore(int id) {
         this.id = id;
+        this.skin = skin;
+    }
+
+    public String getSkin(){
+        return skin = "\uD83D\uDC11";
     }
 }
