bf6e924246344493f4956a06c08169bd305222d5
codleto <codleto@gmail.com>
2025-10-27 17:54:21 +0300

    feat: new constructor in Entity

--- src/Simulation/Entity.java
+++ src/Simulation/Entity.java
@@ -1,6 +1,10 @@
 package Simulation;
 
 public abstract class Entity {
-    public Cordinates cordinates;
+    public Coordinates coordinates; // todo: нельзя хранить координаты в сущности
     public String skin;
+    public String getSkin() {
+        return skin;
+    }
 }
+
