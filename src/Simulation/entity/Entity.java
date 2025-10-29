7bd8064c5a473edaac6018617356abd5b742ed75
codleto <codleto@gmail.com>
2025-10-29 16:56:54 +0300

    refactor(Entity): remove coordinates field from Entity class

---
+++ src/Simulation/entity/Entity.java
@@ -0,0 +1,12 @@
+package Simulation.entity;
+
+import Simulation.Coordinates;
+
+public abstract class Entity {
+    public String skin;
+    public int id;
+    public String getSkin() {
+        return skin;
+    }
+}
+
