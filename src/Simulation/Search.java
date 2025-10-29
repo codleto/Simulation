bcebe04b4bc309fb581a86ad179732f213d7a8ba
codleto <codleto@gmail.com>
2025-10-29 22:56:59 +0300

    feat(Search): add main ALG() method implementing search algorithm

--- src/Simulation/Search.java
+++ src/Simulation/Search.java
@@ -1,5 +1,7 @@
 package Simulation;
 
+import Simulation.entity.Animal.Creature;
+
 import java.util.ArrayList;
 
 public class Search {
@@ -12,6 +14,22 @@ public class Search {
 
     public static boolean izuchKlet(Coordinates coordinates) {
         return !obshiy.contains(coordinates);
+    }
+
+    public static void alg(Map map, Creature creature) {
+        Coordinates startPosition = map.getKey(creature);
+        Coordinates position = map.getKey(creature);
+
+        obshiy.add(startPosition);
+
+        while(true) {
+            if(izuchKlet(position)) {
+                obshiy.add(position);
+                // короче тут если такой координаты в списке нет значит добавлем ее в список и нужно придумать как
+                // добавить эту координату в список и прошлую и что добавлять в putivod ну на листе логика вроде изложена
+
+            }
+        }
     }
 }
 
