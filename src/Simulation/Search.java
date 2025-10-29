1fe2e6018e5a27e757a85031e9baaf3d6b2a0d07
codleto <codleto@gmail.com>
2025-10-29 22:55:38 +0300

    feat(Search): add method estPrepyatstvie() to check for obstacles

--- src/Simulation/Search.java
+++ src/Simulation/Search.java
@@ -5,5 +5,9 @@ import java.util.ArrayList;
 public class Search {
     public static ArrayList<Coordinates> obshiy = new ArrayList<>();
     public static ArrayList<ArrayList<Coordinates>> putivod = new ArrayList<>();
+
+    public static boolean estPrepatstvie(Map map) {
+        return map != null;
+    }
 }
 
