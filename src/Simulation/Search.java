6dbd01ddbc1c20ae4b43ce70b4e51000700a7d82
codleto <codleto@gmail.com>
2025-10-30 21:01:37 +0300

    feat(Search): add main ALG() method implementing search algorithm

--- src/Simulation/Search.java
+++ src/Simulation/Search.java
@@ -1,34 +1,98 @@
 package Simulation;
 
 import Simulation.entity.Animal.Creature;
-
 import java.util.ArrayList;
 
 public class Search {
     public static ArrayList<Coordinates> obshiy = new ArrayList<>();
     public static ArrayList<ArrayList<Coordinates>> putivod = new ArrayList<>();
+    public static ArrayList<Coordinates> inPutevod = new ArrayList<>();
 
-    public static boolean estPrepatstvie(Map map) {
-        return map != null;
+
+
+    public static boolean eda(Coordinates coordinates){
+        return coordinates == null;
     }
+
 
     public static boolean izuchKlet(Coordinates coordinates) {
         return !obshiy.contains(coordinates);
     }
 
+    /*
+  1. понять от куда начинаем
+  - Методу нужно получить ключ , для этого ему нужно знать значение(то есть сущность)
+  - ключ будет стартовой позицией сущности
+   */
+
     public static void alg(Map map, Creature creature) {
         Coordinates startPosition = map.getKey(creature);
-        Coordinates position = map.getKey(creature);
+        Coordinates position;
 
         obshiy.add(startPosition);
 
+        int i = 0;// счетчик нужен для счета внутри массивов и поиска индекса
+        int j = 0; // счетчик для putevod
         while(true) {
-            if(izuchKlet(position)) {
-                obshiy.add(position);
-                // короче тут если такой координаты в списке нет значит добавлем ее в список и нужно придумать как
-                // добавить эту координату в список и прошлую и что добавлять в putivod ну на листе логика вроде изложена
-
+            Coordinates c = obshiy.get(i);
+            position = new Coordinates(c.vertical, c.horizontal + 1);
+            if (eda(position)) {
+                if (izuchKlet(position)) {
+                    obshiy.add(position);
+                    inPutevod.add(startPosition);
+                    inPutevod.add(position);
+                    putivod.add(i, inPutevod);
+                    j++;
+                }
+            } else {
+                j--;
+                putivod.get(j);
             }
+
+            position = new Coordinates(c.vertical + 1, c.horizontal);
+            if (eda(position)) {
+                if (izuchKlet(position)) {
+                    obshiy.add(position);
+                    inPutevod.add(startPosition);
+                    inPutevod.add(position);
+                    putivod.add(i, inPutevod);
+                    j++;
+                }
+            } else {
+                j--;
+                putivod.get(j);
+            }
+
+            position = new Coordinates(c.vertical - 1, c.horizontal);
+            if (eda(position)) {
+                if (izuchKlet(position)) {
+                    obshiy.add(position);
+                    inPutevod.add(startPosition);
+                    inPutevod.add(position);
+                    putivod.add(i, inPutevod);
+                    j++;
+                }
+            } else {
+                j--;
+                putivod.get(j);
+            }
+
+            position = new Coordinates(c.vertical, c.horizontal - 1);
+            if (eda(position)) {
+                if (izuchKlet(position)) {
+                    obshiy.add(position);
+                    inPutevod.add(startPosition);
+                    inPutevod.add(position);
+                    putivod.add(i, inPutevod);
+                    j++;
+                }
+            } else {
+                j--;
+                putivod.get(j);
+                break;
+            }
+
+            i++;
         }
     }
 }
