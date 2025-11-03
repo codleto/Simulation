b0b17a6ddf64b7c0b3835a9c5d9401fdf57358c5
codleto <codleto@gmail.com>
2025-11-03 20:22:45 +0300

    feat(search): add clear() method to reset visited data

--- src/Simulation/Search.java
+++ src/Simulation/Search.java
@@ -1,108 +1,117 @@
 package Simulation;
 
-import Simulation.entity.Animal.Creature;
 import Simulation.entity.StaticObject.Grass;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import static Simulation.Map.granicPole;
 
 public class Search {
-    public static ArrayList<Coordinates> visitedCells = new ArrayList<>();
-    public static HashMap<Coordinates, Coordinates> parentMap = new HashMap<>();
-    public static ArrayList<Coordinates> pathToFood = new ArrayList<>();
+    private Coordinates start;
+    public ArrayList<Coordinates> visitedCells = new ArrayList<>();
+    public HashMap<Coordinates, Coordinates> parentMap = new HashMap<>();
+    public ArrayList<Coordinates> pathToFood = new ArrayList<>();
 
-    public static boolean isCellVisited(Coordinates coordinates) {
+    public void clear(){
+    visitedCells.clear();
+    parentMap.clear();
+    pathToFood.clear();
+}
+
+    public boolean isCellVisited(Coordinates coordinates) {
         return !visitedCells.contains(coordinates);
     }
 
-    public static boolean hasGrass(Map map, Coordinates coordinates){
-        return map.getEntity(coordinates) instanceof Grass;
+    public static boolean hasGrass(Coordinates coordinates){
+        return Map.getEntity(coordinates) instanceof Grass;
     }
 
-    public static void runSearch(Map map, Creature creature) {
-        Coordinates startPosition = creature.coordinates;
-
-        visitedCells.add(startPosition);
+    public void runSearch(Coordinates coordinates) {
+        start = coordinates;
+        visitedCells.add(coordinates);
         int index = 0;
         while(true){
-            Coordinates c = visitedCells.get(index);
-            Coordinates s = new Coordinates(c.horizontal, c.vertical +1);
-            if(!granicPole(s)) {
-                if (map.getMap(s.horizontal, s.vertical) == null) {
-                    if (isCellVisited(s)) {
-                        visitedCells.add(s);
-                        parentMap.put(s, c);
+            if(index >= visitedCells.size()){
+                break;
+            }
+            Coordinates parentCell = visitedCells.get(index)
+                    ;
+            Coordinates childCell = new Coordinates(parentCell.vertical, parentCell.horizontal +1);
+            if(!granicPole(childCell)) {
+                if (Map.getMap(childCell.vertical, childCell.horizontal) == null) {
+                    if (isCellVisited(childCell)) {
+                        visitedCells.add(childCell);
+                        parentMap.put(childCell, parentCell);
                     }
-                } else if (hasGrass(map, s)) {
-                    parentMap.put(s, c);
+                } else if (hasGrass(childCell)) {
+                    parentMap.put(childCell, parentCell);
+                    findPathToFood(childCell);
+                    break;
+                }
+            }
+
+            childCell = new Coordinates(parentCell.vertical + 1, parentCell.horizontal);
+            if(!granicPole(childCell)) {
+                if (Map.getMap(childCell.vertical, childCell.horizontal) == null) {
+                    if (isCellVisited(childCell)) {
+                        visitedCells.add(childCell);
+                        parentMap.put(childCell, parentCell);
+                    }
+                } else if (hasGrass(childCell)) {
+                    parentMap.put(childCell, parentCell);
+                    findPathToFood(childCell);
                     break;
 
                 }
             }
 
-            s = new Coordinates(c.horizontal + 1, c.vertical);
-            if(!granicPole(s)) {
-                if (map.getMap(s.horizontal, s.vertical) == null) {
-                    if (isCellVisited(s)) {
-                        visitedCells.add(s);
-                        parentMap.put(s, c);
+            childCell = new Coordinates(parentCell.vertical, parentCell.horizontal -1);
+            if(!granicPole(childCell)) {
+                if (Map.getMap(childCell.vertical, childCell.horizontal) == null) {
+                    if (isCellVisited(childCell)) {
+                        visitedCells.add(childCell);
+                        parentMap.put(childCell, parentCell);
                     }
-                } else if (hasGrass(map, s)) {
-                    parentMap.put(s, c);
+                } else if (hasGrass(childCell)) {
+                    parentMap.put(childCell, parentCell);
+                    findPathToFood(childCell);
                     break;
 
                 }
             }
 
-            s = new Coordinates(c.horizontal, c.vertical -1);
-            if(!granicPole(s)) {
-                if (map.getMap(s.horizontal, s.vertical) == null) {
-                    if (isCellVisited(s)) {
-                        visitedCells.add(s);
-                        parentMap.put(s, c);
+            childCell = new Coordinates(parentCell.vertical -1, parentCell.horizontal);
+            if(!granicPole(childCell)) {
+                if (Map.getMap(childCell.vertical, childCell.horizontal) == null) {
+                    if (isCellVisited(childCell)) {
+                        visitedCells.add(childCell);
+                        parentMap.put(childCell, parentCell);
                     }
-                } else if (hasGrass(map, s)) {
-                    parentMap.put(s, c);
+                } else if (hasGrass(childCell)) {
+                    parentMap.put(childCell, parentCell);
+                    findPathToFood(childCell);
                     break;
-
                 }
             }
-
-            s = new Coordinates(c.horizontal -1, c.vertical);
-            if(!granicPole(s)) {
-                if (map.getMap(s.horizontal, s.vertical) == null) {
-                    if (isCellVisited(s)) {
-                        visitedCells.add(s);
-                        parentMap.put(s, c);
-                    }
-                } else if (hasGrass(map, s)) {
-                    parentMap.put(s, c);
-                    findPathToFood(s);
-                    break;
-
-                }
-            }
-
             index++;
         }
-
-
-
     }
 
-    public static void findPathToFood(Coordinates coordinates){
+    public  void findPathToFood(Coordinates coordinates){
         pathToFood.add(coordinates);
-        Coordinates c = coordinates;
+        Coordinates childCell = coordinates;
         while(true) {
-            if (!parentMap.containsValue(c)) {
-                Coordinates p = parentMap.get(c);
-                pathToFood.add(p);
-                c = p;
+            if (!start.equals(childCell)) {
+                Coordinates parentCell = parentMap.get(childCell);
+                pathToFood.add(parentCell);
+                childCell = parentCell;
             }else{
+                Collections.reverse(pathToFood);
+                pathToFood.remove(0);
                 break;
             }
         }
-
     }
 }
+
