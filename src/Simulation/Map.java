86aacd29eb4dcda9ca880a6bc2b13eef038479f6
codleto <codleto@gmail.com>
2025-11-03 20:23:16 +0300

    fix(map): fix border check logic and add change() method

--- src/Simulation/Map.java
+++ src/Simulation/Map.java
@@ -1,25 +1,43 @@
 package Simulation;
 
+import Simulation.entity.Animal.Creature;
 import Simulation.entity.Entity;
-
 import java.util.HashMap;
 import java.util.Random;
 
 public class Map {
 
-    Random r = new Random();
-    public HashMap<Coordinates, Entity> map = new HashMap<>();
+    static Random  r = new Random();
 
-    public void setEntity(Entity entity) {
+    public static HashMap<Coordinates, Entity> map = new HashMap<>();
+
+    public static void setEntity(Entity entity) {
         Coordinates c = genPosition();
+        entity.coordinates = c;
         map.put(c, entity);
     }
+    public static Entity getEntity(Coordinates coordinates){
+        return map.get(coordinates);
+    }
 
-    public Entity getMap(int vertical, int horizontal) {
+    public static void chooch(Coordinates oldCoordinates , Coordinates newCoordinates){
+        Entity e = map.get(oldCoordinates);
+        map.remove(oldCoordinates);
+        map.put(newCoordinates, e);
+    }
+
+
+    public static boolean granicPole(Coordinates coordinates) {
+        return (coordinates.vertical > 4 || coordinates.vertical < 0 &&
+            coordinates.horizontal > 4 || coordinates.horizontal < 0);
+    }
+
+    public static Entity getMap(int vertical, int horizontal) {
         return map.get(new Coordinates(vertical, horizontal));
     }
 
-    public Coordinates genPosition() {
+
+    public static Coordinates genPosition() {
         while (true) {
             int vertical = r.nextInt(5);
             int horizontal = r.nextInt(5);
@@ -30,7 +48,7 @@ public class Map {
         }
     }
 
-    public String getSkins(int vertical, int horizontal) {
+    public static String getSkins(int vertical, int horizontal) {
         Entity skin = map.get(new Coordinates(vertical, horizontal));
         return skin.getSkin();
     }
