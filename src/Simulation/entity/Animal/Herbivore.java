86f30aa4f19c95211452fd22b68303ef6c7e2ca9
codleto <codleto@gmail.com>
2025-11-04 22:55:29 +0300

    feat(herbivore): add method eat

--- src/Simulation/entity/Animal/Herbivore.java
+++ src/Simulation/entity/Animal/Herbivore.java
@@ -1,28 +1,34 @@
 package Simulation.entity.Animal;
+import Simulation.Coordinates;
 import Simulation.Map;
 import Simulation.Search;
+import Simulation.entity.StaticObject.Grass;
 
 public class Herbivore extends Creature {
     private int stepIndex = 0;
 
     public Herbivore(int id) {
         this.id = id;
-        this.skin = "\uD83D\uDC11";
+        super.setSkin("\uD83D\uDC11");
     }
     Search search = new Search();
 
+    public boolean eat(Coordinates coordinates){
+        return Map.getEntity(coordinates) instanceof Grass;
+    }
+
     public void makeMove() {
-        while (true) {
+        while(true) {
             if (stepIndex == 0) {
-                search.runSearch(this.coordinates);
+                search.runSearch(this.coordinates, this);
             }
 
             if (stepIndex >= search.pathToFood.size()) {
                 search.clear();
                 stepIndex = 0;
-                continue;
+                return;
             }
-            Map.chooch(this.coordinates, search.pathToFood.get(stepIndex));
+            Map.chooch(this.coordinates, search.pathToFood.get(stepIndex), this);
             this.coordinates = search.pathToFood.get(stepIndex);
             stepIndex++;
             break;
