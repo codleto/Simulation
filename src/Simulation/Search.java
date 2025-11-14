c632c16e3a171ecd797d85c2f1773eec37a6ecbd
codleto <codleto@gmail.com>
2025-11-14 12:00:27 +0300

    refactor: reduce Search method complexity without changing behavior

--- src/Simulation/Search.java
+++ src/Simulation/Search.java
@@ -1,104 +1,69 @@
 package Simulation;
 
 import Simulation.entity.Animal.Creature;
+import Simulation.entity.StaticObject.Grass;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 
-import static Simulation.Map.hasFoodFor;
 import static Simulation.Map.granicPole;
 
 public class Search {
-    private Coordinates start;
+    private Coordinates startCell;
     public ArrayList<Coordinates> visitedCells = new ArrayList<>();
-    public HashMap<Coordinates, Coordinates> parentMap = new HashMap<>();
+    public HashMap<Coordinates, Coordinates> pathToStartCell = new HashMap<>();
     public ArrayList<Coordinates> pathToFood = new ArrayList<>();
     public static ArrayList<Integer> breakk = new ArrayList<>();
+    public ArrayList<Integer> grass = new ArrayList<>();
 
     public void clear() {
         visitedCells.clear();
-        parentMap.clear();
+        pathToStartCell.clear();
         pathToFood.clear();
-        start = null;
+        startCell = null;
+        grass.clear();
     }
 
     public boolean isCellNotVisited(Coordinates coordinates) {
         return !visitedCells.contains(coordinates);
     }
 
-
     public void runSearch(Coordinates coordinates, Creature creature) {
-        start = coordinates;
+        startCell = coordinates;
         visitedCells.add(coordinates);
-        int index = 0;
-        while (true) {
 
+        int index = 0;
+        boolean go = true;
+        while (go) {
             if (index >= visitedCells.size()) { // если индекс равен или больше размера посещенных клеток
                 break;
             }
+
             Coordinates parentCell = visitedCells.get(index);
-            Coordinates childCell = new Coordinates(parentCell.vertical, parentCell.horizontal + 1);
-            if (!granicPole(childCell)) {
-                if (Map.getMap(childCell.vertical, childCell.horizontal) == null) { // если клетка пустая
-                    if (isCellNotVisited(childCell)) { // если клетку не посещали
-                        visitedCells.add(childCell); // начинаем запись в очередь
-                        parentMap.put(childCell, parentCell);
-                    }
-                } else if (creature.eat(Map.getEntity(childCell))) { // если клетка еда
-                    parentMap.put(childCell, parentCell);
-                    findPathToFood(childCell);
-                    break;
-                } else if(!creature.eat(Map.getEntity(childCell))){ // если клетка не еда
-                    break;
-                }
-            }
+            Coordinates[] childCell = {
+                    new Coordinates(parentCell.vertical, parentCell.horizontal + 1),
+                    new Coordinates(parentCell.vertical + 1, parentCell.horizontal),
+                    new Coordinates(parentCell.vertical, parentCell.horizontal - 1),
+                    new Coordinates(parentCell.vertical - 1, parentCell.horizontal)
+            };
 
-            childCell = new Coordinates(parentCell.vertical + 1, parentCell.horizontal);
-            if (!granicPole(childCell)) {
-                if (Map.getMap(childCell.vertical, childCell.horizontal) == null) {
-                    if (isCellNotVisited(childCell)) {
-                        visitedCells.add(childCell);
-                        parentMap.put(childCell, parentCell);
-                    }
-                } else if (creature.eat(Map.getEntity(childCell))) {
-                    parentMap.put(childCell, parentCell);
-                    findPathToFood(childCell);
-                    break;
-                } else if(!creature.eat(Map.getEntity(childCell))) {
-                    break;
-                }
-            }
+            for (Coordinates child : childCell) {
+                if (!granicPole(child)) {
+                    if (Map.getMap(child) == null) { // если клетка пустая
+                        if (isCellNotVisited(child)) { // если клетку не посещали
+                            visitedCells.add(child); // начинаем запись в очередь
+                            pathToStartCell.put(child, parentCell);
+                        }
+                    } else if (creature.eat(Map.getEntity(child))) { // если клетка еда
+                        pathToStartCell.put(child, parentCell);
+                        findPathToFood(child);
+                        go = false;
+                        break;
 
-            childCell = new Coordinates(parentCell.vertical, parentCell.horizontal - 1);
-            if (!granicPole(childCell)) {
-                if (Map.getMap(childCell.vertical, childCell.horizontal) == null) {
-                    if (isCellNotVisited(childCell)) {
-                        visitedCells.add(childCell);
-                        parentMap.put(childCell, parentCell);
+                    } else if(Map.getEntity(child) instanceof Grass ) {
+                        grass.add(1);
                     }
-                } else if (creature.eat(Map.getEntity(childCell))) {
-                    parentMap.put(childCell, parentCell);
-                    findPathToFood(childCell);
-                    break;
-                } else if(!creature.eat(Map.getEntity(childCell))){
-                    break;
-                }
-            }
-
-            childCell = new Coordinates(parentCell.vertical - 1, parentCell.horizontal);
-            if (!granicPole(childCell)) {
-                if (Map.getMap(childCell.vertical, childCell.horizontal) == null) {
-                    if (isCellNotVisited(childCell)) {
-                        visitedCells.add(childCell);
-                        parentMap.put(childCell, parentCell);
-                    }
-                } else if (creature.eat(Map.getEntity(childCell))) {
-                    parentMap.put(childCell, parentCell);
-                    findPathToFood(childCell);
-                    break;
-                } else if(!creature.eat(Map.getEntity(childCell))){
-                    break;
                 }
             }
             index++;
@@ -108,12 +73,14 @@ public class Search {
     public void findPathToFood(Coordinates coordinates) {
         pathToFood.clear();
         Coordinates childCell = coordinates;
+
         while(true) {
-            if (childCell != null && !start.equals(childCell)) {
+            if (childCell != null && !startCell.equals(childCell)) {
                 pathToFood.add(childCell);
-                childCell = parentMap.get(childCell);
+                childCell = pathToStartCell.get(childCell);
             }
-            if(start.equals(childCell)){
+
+            if(startCell.equals(childCell)){
                 break;
             }
 
@@ -125,4 +92,3 @@ public class Search {
         Collections.reverse(pathToFood);
     }
 }
-// теперь первый элемент — следующий шаг
