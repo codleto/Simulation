e0114d1eb6843e2d9d0a455a881563ca44384a3e
codleto <codleto@gmail.com>
2026-02-02 13:37:25 +0300

    refactor: make GameConfig immutable with private final fields

--- src/simulation/action/initaction/GameConfig.java
+++ src/simulation/action/initaction/GameConfig.java
@@ -2,7 +2,31 @@ package simulation.action.initaction;
 
 public class GameConfig {
 
-    public static int sheepCount;
-    public static int wolfAttack;
-    public static int wolfSpeed;
+    private final int sheepCount;
+    private final int wolfAttack;
+    private final int wolfSpeed;
+
+    public GameConfig(int sheepCount, int wolfAttack, int wolfSpeed){
+        if(sheepCount > 0 && wolfAttack > 0 && wolfSpeed > 0){
+            this.sheepCount = sheepCount;
+            this.wolfAttack = wolfAttack;
+            this.wolfSpeed = wolfSpeed;
+        } else {
+            this.sheepCount = 1;
+            this.wolfAttack = 1;
+            this.wolfSpeed = 1;
+        }
+    }
+
+    public int getSheepCount(){
+        return sheepCount;
+    }
+
+    public int getWolfAttack(){
+        return wolfAttack;
+    }
+
+    public int getWolfSpeed(){
+        return wolfSpeed;
+    }
 }
